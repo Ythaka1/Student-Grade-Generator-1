98bbaa37c0f26b4df9a16a3e0a7d2220c7d1608c
Hakim Waithaka <hakim.waithaka@gmail.com>
2024-09-28 21:08:55 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let marks = (69)
+
+
+let grade;
+if(marks > 79) {
+    console.log("A");
+}else if (marks >=60) {
+    console.log("B");
+}else if (marks >= 49) {
+    console.log("c");
+}else if (marks >= 40) {
+    console.log("D");
+    
+}else {
+    grade =E
+}
